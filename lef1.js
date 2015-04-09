81d4728b6ee2d351ef528b84ce93fddc00d082d1
Alberto Garcia de Marina <1989gmg@gmail.com>
2015-04-09 15:03:14 +0200

    Añadida funcion busqueda

--- lef1.js
+++ lef1.js
@@ -1,59 +1,78 @@
-$(document).ready(function(){
-	var map = L.map('map').locate({setView: true, maxZoom: 17});
-	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	}).addTo(map);
+var map = L.map('map').locate({setView: true, maxZoom: 17});
 
-	L.marker([40.2838, -3.8215]).addTo(map)
-    .bindPopup('Aulario III, URJC')
-    .openPopup();
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
-    map.on('click', function(e) {
-    	alert(e.latlng);
-	});
+L.marker([40.2838, -3.8215]).addTo(map)
+.bindPopup('Aulario III, URJC')
+.openPopup();
 
-    // Define one GeoJSON feature
-    var geojsonFeature = {
-	"type": "Feature",
-	"properties": {
-            "name": "Aulario III",
-            "amenity": "Classrooms Building",
-            "popupContent": "Most of the classes of ETSIT are taught here."
-	},
-	"geometry": {
-            "type": "Point",
-            "coordinates": [-3.8215, 40.2838]
+map.on('click', function(e) {
+	alert(e.latlng);
+});
+
+function popUpName(feature, layer) {
+if (feature.properties && feature.properties.Name) {
+        layer.bindPopup(feature.properties.Name);
 	}
-    };
+}
 
-    // Define a function to show the name property
-    function popUpName(feature, layer) {
-	// does this feature have a property named popupContent?
-	if (feature.properties && feature.properties.Name) {
-            layer.bindPopup(feature.properties.Name);
-	}
-    }
-
-    // Add to map a layer with the geojsonFeature point
-    var myLayer = L.geoJson().addTo(map);
-    myLayer.addData(geojsonFeature);
-
-    // Add to map a layer with all points in buildings-urjc.json
-    $.getJSON("edificios.json", function(data) {
+$.getJSON("edificios.json", function(data) {
 	buildingsLayer = L.geoJson(data, {
 	    onEachFeature: popUpName
 	}).addTo(map);
-    });
-
-
-	function onLocationFound(e) {
-	    var radius = e.accuracy / 2;
-
-	    L.marker(e.latlng).addTo(map)
-	        .bindPopup("Estas en un area de " + radius + " metros desde punto.").openPopup();
-
-	    L.circle(e.latlng, radius).addTo(map);
-	}
-	map.on('locationfound', onLocationFound);
-
 });
+
+function onLocationFound(e) {
+    var radius = e.accuracy / 2;
+
+    L.marker(e.latlng).addTo(map)
+        .bindPopup("Estas en un area de " + radius + " metros desde punto.").openPopup();
+
+    L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function addr_search(){
+	var inp = document.getElementById("addr");
+
+	$.getJSON('http://nominatim.openstreetmap.org/search?format=json&limit=5&q=' + inp.value, function(data) {
+	var items = [];
+
+	$.each(data, function(key, val) {
+		  items.push(
+		    "<li><a href='#' onclick='chooseAddr(" +
+		    val.lat + ", " + val.lon + ");return false;'>" + val.display_name +
+		    '</a></li>'
+		  );
+		});
+		$('#results').empty();
+		if (items.length != 0) {
+			$('<p>', { html: "Search results:" }).appendTo('#results');
+			$('<ul/>', {
+			'class': 'my-new-list',
+			html: items.join('')
+			}).appendTo('#results');
+		} else {
+			$('<p>', { html: "No results found" }).appendTo('#results');
+		}
+	});
+}
+
+function chooseAddr(lat, lng, type) {
+	var location = new L.LatLng(lat, lng);
+	map.panTo(location);
+
+	if (type == 'city' || type == 'administrative') {
+	map.setZoom(11);
+	} else {
+	map.setZoom(13);
+	}
+}
+
+function reset_results(){
+	document.getElementById("results").innerHTML = "";
+	document.getElementById("addr").value = "";
+}
